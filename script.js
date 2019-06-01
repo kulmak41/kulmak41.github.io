44971c4d0f92263c666096e70bfbf1553da8f5a1
kulmak41 <49606595+kulmak41@users.noreply.github.com>
2019-06-01 22:57:21 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -3,8 +3,16 @@ menu_btn.addEventListener("click", function() {
   menu_btn_icon.classList.toggle("fa-bars");
   menu_btn_icon.classList.toggle("fa-times");
 });
+
 window.addEventListener("click", function(event) {
-  if(event.target != header_stripe) {
+  if (
+    event.target != header_stripe &&
+    event.target != menu_btn_icon &&
+    event.target != menu_btn &&
+    header_stripe.classList.contains("menu_is-open")
+  ) {
     header_stripe.classList.toggle("menu_is-open");
+    menu_btn_icon.classList.toggle("fa-bars");
+    menu_btn_icon.classList.toggle("fa-times");
   }
-});
+})
